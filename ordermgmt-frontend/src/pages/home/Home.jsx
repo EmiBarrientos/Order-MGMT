d662f6fb80234c3a2ea644ab9800c62bff46afc8
emiliano <emilianogbarrientos@gmail.com>
2026-04-09 10:15:11 -0300

    eliminando carpeta front/separando front de back

--- ordermgmt-frontend/src/pages/home/Home.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-6 text-white">
-      <h1 className="text-2xl font-bold">Home</h1>
-    </div>
-  );
-}
--- ordermgmt-frontend/src/services/orders.js
+++
@@ -1,5 +0,0 @@
-import { apiGet } from "./api/client";
-
-export function getAllOrders() {
-  return apiGet("/api/order/with-products");
-}
